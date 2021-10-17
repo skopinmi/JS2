a0246ad8553ac8ea2060a997bd0d736242cfa057
skopinmi <64150928+skopinmi@users.noreply.github.com>
2021-10-17 22:57:38 +0300

    hw8

---
+++ cart.js
@@ -0,0 +1,50 @@
+let cart = {
+    data() {
+      return {
+        totalPrice: 'не работает',
+        isVisible: false
+      }
+    },
+    template: `
+      <div class="cart">
+        <button class="cart-button" type="button" v-on:click=changeVisible() v-show="isVisible">Скрыть корзину</button>
+        <button class="cart-button" type="button" v-on:click=changeVisible() v-show="!isVisible">Показать корзину</button>
+        <div  v-show="isVisible">
+          <table>
+            <thead>
+              <tr>
+                <th>Название товара</th>
+                <th>Количество</th>
+                <th>Цена за шт.</th>
+                <th>Итого</th>
+                <th>Действие</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in $root.goodsInCart">
+                <td>{{item.product.product_name}}</td>
+                <td>{{item.count}}</td>
+                <td>{{item.product.price}}</td>
+                <td>{{item.count * item.product.price}}</td>
+                <td><button type="button" v-on:click=$root.deleteFromCart(item.product)>-</button></td>
+              </tr>
+            </tbody>
+          </table>
+          <table class="totalprice">
+            <tr>
+                <th>Товаров в корзине на сумму:</th>
+                <th id="total">{{totalPrice}}</th>
+            </tr>
+          </table>
+        </div>
+      </div>  
+    `,
+    methods: {
+      changeVisible(){
+        this.isVisible = this.isVisible == false;
+      }
+    }
+  };
+  export default {
+    cart: cart
+  };  
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict'
 
+import Cart from 'cart.js';
+
 const postResponse = async (url, data) => {
   return await fetch(url, {
     method: 'POST',
@@ -20,6 +22,9 @@ const app = new Vue({
     filteredGoods: [],
     goodsInCart: [],
     totalPrice: 0,
+  },
+  components: {
+    'cart': Cart.cart
   },
   methods: {
     makeGETRequest(url) {
@@ -105,54 +110,6 @@ Vue.component('goods-search', {
       this.$root.filteredGoods = this.$root.goods.filter(good => regexp.test(good.product_name));
     }
   } 
-});
-
-Vue.component('cart', {
-  data() {
-    return {
-      totalPrice: 'не работает',
-      isVisible: false
-    }
-  },
-  template: `
-    <div class="cart">
-      <button class="cart-button" type="button" v-on:click=changeVisible() v-show="isVisible">Скрыть корзину</button>
-      <button class="cart-button" type="button" v-on:click=changeVisible() v-show="!isVisible">Показать корзину</button>
-      <div  v-show="isVisible">
-        <table>
-          <thead>
-            <tr>
-              <th>Название товара</th>
-              <th>Количество</th>
-              <th>Цена за шт.</th>
-              <th>Итого</th>
-              <th>Действие</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="item in $root.goodsInCart">
-              <td>{{item.product.product_name}}</td>
-              <td>{{item.count}}</td>
-              <td>{{item.product.price}}</td>
-              <td>{{item.count * item.product.price}}</td>
-              <td><button type="button" v-on:click=$root.deleteFromCart(item.product)>-</button></td>
-            </tr>
-          </tbody>
-        </table>
-        <table class="totalprice">
-          <tr>
-              <th>Товаров в корзине на сумму:</th>
-              <th id="total">{{totalPrice}}</th>
-          </tr>
-        </table>
-      </div>
-    </div>  
-  `,
-  methods: {
-    changeVisible(){
-      this.isVisible = this.isVisible == false;
-    }
-  }
 });
 
 Vue.component('goods-list', {
